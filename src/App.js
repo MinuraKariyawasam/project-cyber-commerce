f2dc5c8593dc1f5b38f075cfed8ad1d38017d652
Adee99 <adee99@users.noreply.github.com>
2021-01-27 13:54:36 +0530

    App Navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import Products from './components/Products/Products';
+//import Products from './components/Products/Products';
+//import Navbar from './components/Navbar/Navbar';
+
+import {Products, Navbar} from './components';
+
 
 const App = () => {
     return (
         <div>
+            <Navbar />
             < Products />
         </div>
     )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {AppBar, Toolbar,IconButton,Badge,MenuItem,Menu, Typography} from '@material-ui/core';
+import {ShoppingCart } from '@material-ui/icons';
+//import classes from '*.module.css';
+import logo from '../../assets/comz.jpg';
+import useStyles from './styles';
+
+const Navbar = () => {
+    const classes = useStyles();
+    return (
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color='inherit'>
+                        <img src={logo} alt= "TeamCyberCommerce" height="25px" className={classes.image} >
+                            TeamCyberCommerce
+                        </img>
+                    </Typography>
+                    <div className={classes.grow}/>
+                    <div className={classes.button}>
+                        <IconButton aria-label="show cart items" color="inherit">
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart/>
+                            </Badge>
+
+                        </IconButton>
+                    </div>
+                </Toolbar>
+
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar

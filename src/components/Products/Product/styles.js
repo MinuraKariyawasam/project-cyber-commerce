9e0a00c7333a3b70e564d4e21cf17ea5b87163d5
Minura Kariyawasam <minura.20191020@iit.ac.lk>
2021-01-27 12:04:43 +0530

    add basic styles to product component

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -2,6 +2,18 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
     root: {
-        maxWidth: '100%'
-    }
-}))
+        maxWidth: '100%',
+      },
+      media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+      },
+      cardActions: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+      },
+      cardContent: {
+        display: 'flex',
+        justifyContent: 'space-between',
+      },
+}));

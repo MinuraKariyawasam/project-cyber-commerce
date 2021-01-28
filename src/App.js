c823b546037fa2fe340e6bcb8d209274d4e1ce2d
Adee99 <adee99@users.noreply.github.com>
2021-01-28 18:53:21 +0530

    Backend API & part of the cart done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
+import {commerce} from './lib/commerce'; //instance to handle backend calls
 
 //import Products from './components/Products/Products';
 //import Navbar from './components/Navbar/Navbar';
 
-import {Products, Navbar} from './components';
-
+import {Products, Navbar,Cart} from './components';
 
 const App = () => {
-    return (
-        <div>
-            <Navbar />
-            < Products />
-        </div>
-    )
-}
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
 
-export default App
+    const fetchProducts = async()=>{
+        const { data } = await commerce.products.list(); //api call
+
+        setProducts(data);
+    };
+
+    //cart
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve());
+      };
+    
+      const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+    
+        setCart(item.cart);
+      };
+    //   const refreshCart = async () => {
+    //     const newCart = await commerce.cart.refresh();
+    
+    //     setCart(newCart);
+    //   };
+      useEffect(() => {
+        fetchProducts();
+        fetchCart();
+      }, []);
+    
+      return (
+          <div >
+    
+            <Navbar />
+            <Products products={products} onAddToCart={handleAddToCart} handleUpdateCartQty />
+
+          </div>
+      );
+    };
+    
+    export default App;
